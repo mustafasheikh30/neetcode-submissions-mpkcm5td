1fef2ebddfae2d44615600e2ac9e383a82e8c17c
Mustafa Sheikh <mustafasheikh188@gmail.com>
2026-04-22 16:35:29 +0530

    Add: string-matching-in-an-array - submission-0

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List result = new ArrayList();
+        for(int i =0;i<words.length;i++) {
+            for(int j=0;j<words.length;j++){
+            if(words[j].contains(words[i])&&i!=j){
+                result.add(words[i]);
+                break;
+            }
+            }
+        }
+        return result;
+    }
+}
