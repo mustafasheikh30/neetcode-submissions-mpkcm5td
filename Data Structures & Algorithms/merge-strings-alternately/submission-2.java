1b0d079a53ea608e6fd8ef0bee86e72620ecb9b1
Mustafa Sheikh <mustafasheikh188@gmail.com>
2026-04-24 10:33:41 +0530

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder(word1.length() + word2.length());
+        int i = 0;
+        while (i < word1.length() && i < word2.length()) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+            i++;
+        }
+        // appending remaining part
+        while (i < word1.length()) {
+            sb.append(word1.charAt(i++));
+        }
+        while (i < word2.length()) {
+            sb.append(word2.charAt(i++));
+        }
+
+        return sb.toString();
+    }
+}
